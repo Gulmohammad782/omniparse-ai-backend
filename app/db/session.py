2e810b6c236971dbcb5f02403dd20469692e4333
Gulmohammad782 <gulmohammad8920@gmail.com>
2026-09-05 13:56:07 +0530

    Update configuration and requirements

--- app/db/session.py
+++ app/db/session.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Update with your local MySQL credentials: username:password@host:port/database
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/omniparse_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
